c1dc7198ae789fa78bd6e4d58b1bee5cdd2bdf7c
Sid <siddharthsakre@gmail.com>
2024-03-12 13:01:36 +0530

    Protobuf version upgrade

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/sid/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sid/Documents/SVGAPlayer-Flutter/example"
+export "FLUTTER_APPLICATION_PATH=/Users/sid/Documents/other_repos/SVGAPlayer-Flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
